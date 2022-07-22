a7b5809aeb3a2ff24694044523590aeb4c1525bb
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-22 18:46:28 -0500

    backend

--- AppHeores/src/app/app.module.ts
+++ AppHeores/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
+import { HolaComponent } from './hola/hola.component';
 
 @NgModule({
-  declarations: [AppComponent, ErrorPageComponent],
+  declarations: [AppComponent, ErrorPageComponent, HolaComponent],
   imports: [
     AppRoutingModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
